85f59f7644a6957c47cbaca0d971613f529252de
kflo-m <kjetil@flovild-midtlie.name>
2013-12-06 15:49:41 +0100

    ready for some testing

--- main.js
+++ main.js
@@ -4,16 +4,24 @@ var xml2js = require('xml2js'),
     name = 'x2js',
     optimist = require('optimist'),
     argv = optimist
-    .options('help',{alias:'h', describe: 'Show help'})
-    .options('caproot',{alias:'c',describe:'no root elem in JSON (if just one)'})
-    .usage('Reads xml input and writes to standard output.\nUsage : ' + name + ' [--help] [--caproot] FILE|-')
-    .argv,
+      .options('help',
+        {alias:'h', describe: 'Show help'})
+      .options('indents',
+        {alias:'i', 
+         describe: "Indents. Num of spaces",
+         default: 2})
+      .options('encoding',
+        {alias:'e',
+         describe: 'encoding if using stdin',
+         default:'utf8'})
+      .options('noroot',
+        {alias:'n',describe: 'strip root elem in output'})
+      .usage('Reads xml and writes to standard output.\nUsage : ' +
+        name + ' [--help] [--noroot] FILE|-')
+      .argv,
     fileParam = argv._[0],
     parser = new xml2js.Parser(),
     filePath;
-
-
-    
 
 if ( !fileParam || argv.help ){
   optimist.showHelp();
@@ -21,15 +29,23 @@ if ( !fileParam || argv.help ){
 }
 
 parser.addListener('end', function(result) {
-    console.dir(result);
-    console.log('Done.');
+  console.log( JSON.stringify(result,null,2));
 });
 
 
 if ( fileParam === '-' ){
   // stdin
+  process.stdin.resume();
+  process.stdin.setEncoding(argv.encoding);
+  xmlBuf="";
+  process.stdin.on('data', function (chunk) {
+    xmlBuf = xmlBuf + chunk;
+  });
+  process.stdin.on('end',function(){
+    parser.parseString(xmlBuf);
+  });
 } else {
-  filePath = path.normalize(path.join(__dirname,fileParam));
+  filePath = path.resolve(__dirname,fileParam);
   fs.readFile(filePath, function(err, data) {
     parser.parseString(data);
   });
